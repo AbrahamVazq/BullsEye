ff3b5721865613499a5d347b044f100218248ad8
Abraham Vazquez <mabraham.vazquez@gmail.com>
2016-12-30 04:16:08 -0600

    Creación del AboutViewController
    viewcontroller con la información del juego y un botón de cerrar que
    nos lleva a la pantalla del juego
    
    Se agrega al proyecto como un .swift y se agrega en el Storyboard para
    sus conexiones por medio de un segué -  modal

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Abraham Vazquez on 12/30/16.
+//  Copyright © 2016 Abraham Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cerrar()
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
